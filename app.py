4e31c49f3015675df1f7ad10a322efc0ecd376d3
Isael Aaron Segoviano Caudillo <143030185+Taquitoo3000@users.noreply.github.com>
2025-11-19 15:39:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -688,6 +688,5 @@ def crear_mapa_vacio(mensaje):
 
 # Para produccion
 if __name__ == '__main__':
-    import os
-    debug = os.environ.get('DEBUG', 'False').lower() == 'true'
-    app.run_server(debug=debug, host='0.0.0.0', port=int(os.environ.get('PORT', 8050)))
+    app.run_server(debug=True, port=int(os.environ.get('PORT', 8050)))
+
